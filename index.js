31c7d66411873a4035e8a04737708f4351fd1c46
Ilya <litovchic@gmail.com>
2019-04-12 14:58:59 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const TelegramBot = require('node-telegram-bot-api')
+const TOKEN = '854738435:AAEwuK56lcRKBz28g7t7lsRH3yyJJcMsRPY';
+const bot = new TelegramBot(TOKEN, {polling:true})
+bot.on('message', msg => {
+    bot.sendMessage(msg.chat.id, `Hello from HEROKU. ${msg.from.first_name}`)
+})
