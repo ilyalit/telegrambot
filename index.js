06ec915f1bbe16a1dcab3313185b14a846c54f25
Ilya <litovchic@gmail.com>
2019-04-15 12:02:25 +0300

    Ready to run on heroku

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-const TelegramBot = require('node-telegram-bot-api')
-const TOKEN = '854738435:AAEwuK56lcRKBz28g7t7lsRH3yyJJcMsRPY';
-const bot = new TelegramBot(TOKEN, {
-    webHook:{
-        port: process.env.PORT
-    }})
+// const TelegramBot = require('node-telegram-bot-api')
+// const TOKEN = '854738435:AAEwuK56lcRKBz28g7t7lsRH3yyJJcMsRPY';
+// const bot = new TelegramBot(TOKEN, {
+//     webHook:{
+//         port: process.env.PORT
+//     }})
 
-bot.setWebHook(`https://echo-webhook.herokuapp.com/`+TOKEN)
+// bot.setWebHook(`https://echo-webhook.herokuapp.com/`+TOKEN)
 
-bot.on('message', msg => {
-    bot.sendMessage(msg.chat.id, `Hello from HEROKU. ${msg.from.first_name}`)
-})
+// bot.on('message', msg => {
+//     bot.sendMessage(msg.chat.id, `Hello from HEROKU. ${msg.from.first_name}`)
+// })
+require("dotenv").config();
+
+var bot = require('./bot');
+require('./web')(bot);
