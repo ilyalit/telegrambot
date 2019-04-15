0c33b3efd4fa70a7d52d2c2ed8e4108e19653b5f
Ilya <litovchic@gmail.com>
2019-04-15 10:48:10 +0300

    set webhook

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const TelegramBot = require('node-telegram-bot-api')
 const TOKEN = '854738435:AAEwuK56lcRKBz28g7t7lsRH3yyJJcMsRPY';
-const bot = new TelegramBot(TOKEN)
+const bot = new TelegramBot(TOKEN, {polling:true})
 
-bot.setWebHook(`https://echo-webhook.herokuapp.com/`+TOKEN)
+//bot.setWebHook(`https://echo-webhook.herokuapp.com/`+TOKEN)
 
 bot.on('message', msg => {
     bot.sendMessage(msg.chat.id, `Hello from HEROKU. ${msg.from.first_name}`)
